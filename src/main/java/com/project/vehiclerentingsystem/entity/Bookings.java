a15de0483a5a18f85173cf2d711242529e149070
Manish Kumar <110426424+Mkkr1998@users.noreply.github.com>
2024-03-19 02:17:57 +0530

    updated entities

--- src/main/java/com/project/vehiclerentingsystem/entity/Bookings.java
+++ src/main/java/com/project/vehiclerentingsystem/entity/Bookings.java
@@ -1,5 +1,28 @@
 package com.project.vehiclerentingsystem.entity;
 
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class Bookings {
+    @Id
+    private int bookingId;
+    @JoinColumn(name = "branch_id")
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Branch branch;
+    @JoinColumn(name = "vehicle_id")
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Vehicle vehicle;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 }
